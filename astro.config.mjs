4b4f340bbbed7ef1d5f471c27ebceacc8256fd0a
Daniel hernandez galvez <hernandezgalvezalejandro@gmail.com>
2025-10-18 11:44:42 -0600

    preparation section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [astroIcon()],
     vite: {
         plugins: [tailwindcss()]
     }
